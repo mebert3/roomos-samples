8242597f9f5b53a939fc9a34ec4da0dfc1a5f1e5
mebert3 <mebert@cisco.com>
2024-12-06 14:28:50 +0100

    Add files via upload

---
+++ kiosk_simple/src/main.js
@@ -0,0 +1,42 @@
+
+function setup() {
+
+  Alpine.store('model', {
+    currentPage: 'home', // 'home', 'service'
+    currentLanguage: 'english',
+    dialNumber1: 'mebert@cisco.com',
+    dialNumber2: 'erica.talking@ivr.vc',
+    dialNumber3: 'ana.suopelto@br.bosch.com',  
+    services: [],
+
+    init() {
+      const params = new URLSearchParams(location.search);
+      if (params.has('number')) {
+        this.dialNumber = params.get('number');
+      }
+      this.services = [
+        { url: this.dialNumber1, name: 'TI' },
+        { url: this.dialNumber2, name: 'RH' },
+        { url: this.dialNumber3, name: 'FCM' },
+      ];
+    },
+    get page() {
+      return this.currentPage;
+    },
+    set page(nextPage) {
+      this.currentPage = nextPage;
+    },
+    currentLanguage: 'english',
+    languages: ['english'],
+    get language() {
+      return this.currentLanguage;
+    },
+    set language(current) {
+      this.currentLanguage = current;
+    },
+  });
+
+}
+
+document.addEventListener('alpine:init', setup);
+
